23b07fb48d3447b612a2b3fd9ccb58f98e6b5ac3
Tamara Miljevic <miljevictamara@gmail.com>
2024-03-27 11:02:13 +0100

    feat: home page created.

--- front-end/pki-system/src/app/app.component.ts
+++ front-end/pki-system/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCommonModule } from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './layout/home/home.component';
+import { ServicesComponent } from './layout/services/services.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatCommonModule, MatButtonModule, HomeComponent, ServicesComponent,
+  FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ front-end/pki-system/src/app/layout/layout.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    ServicesComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomeComponent,
+    ServicesComponent,
+    FooterComponent
+  ]
+})
+export class LayoutModule { }

dffc233332073c9e78e4b9bd87927e009d44ff97
Nina <nina.batranovic3@gmail.com>
2024-03-29 11:08:27 +0100

    added @Setter and @Getter

--- back-end/pki-system/src/main/java/com/example/demo/model/IssuerData.java
+++ back-end/pki-system/src/main/java/com/example/demo/model/IssuerData.java
@@ -1,11 +1,15 @@
 package com.example.demo.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bouncycastle.asn1.x500.X500Name;
 
 import java.security.PrivateKey;
 
 
 
+@Setter
+@Getter
 public class IssuerData {
 
     private X500Name x500name;
@@ -17,22 +21,6 @@ public class IssuerData {
     public IssuerData(PrivateKey privateKey, X500Name x500name) {
         this.privateKey = privateKey;
         this.x500name = x500name;
-    }
-
-    public X500Name getX500name() {
-        return x500name;
-    }
-
-    public void setX500name(X500Name x500name) {
-        this.x500name = x500name;
-    }
-
-    public PrivateKey getPrivateKey() {
-        return privateKey;
-    }
-
-    public void setPrivateKey(PrivateKey privateKey) {
-        this.privateKey = privateKey;
     }
 
 }

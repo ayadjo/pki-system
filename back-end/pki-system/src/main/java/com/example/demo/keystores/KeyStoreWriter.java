8b16e12d22aa3f2cb6d97b18861ddba0e244314a
Tamara Miljevic <miljevictamara@gmail.com>
2024-04-01 13:51:23 +0200

    fix: certificate form changed

--- back-end/pki-system/src/main/java/com/example/demo/keystores/KeyStoreWriter.java
+++ back-end/pki-system/src/main/java/com/example/demo/keystores/KeyStoreWriter.java
@@ -1,12 +1,11 @@
 package com.example.demo.keystores;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import javax.security.auth.DestroyFailedException;
+import java.io.*;
 import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -74,30 +73,6 @@ public class KeyStoreWriter {
             e.printStackTrace();
         }
     }
-
-    public void writeEE(String alias, PrivateKey privateKey, char[] password, Certificate[] certificateChain) {
-        try {
-            keyStore.setKeyEntry(alias, privateKey, password, certificateChain);
-            X509Certificate certificate=(X509Certificate)keyStore.getCertificate("alias");
-        } catch (KeyStoreException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /*public void setCertificateChain(String alias, Certificate[] certificateChain, Certificate subjectCertificate) {
-        try {
-            List<Certificate> newChainList = new ArrayList<>(Arrays.asList(certificateChain));
-            newChainList.add(0, subjectCertificate);
-            Collections.reverse(newChainList);
-
-            for (int i = 0; i < newChainList.size() - 1; i++) {
-                keyStore.setCertificateEntry(alias, newChainList.get(i));
-            }
-        } catch (KeyStoreException e) {
-            e.printStackTrace();
-        }
-    }*/
-
 
 
 

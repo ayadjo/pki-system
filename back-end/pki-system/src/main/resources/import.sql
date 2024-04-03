5563f64fc3c7d3007a480ad21b85816f67efe1e5
adrian-antonic <116621818+adrian-antonic@users.noreply.github.com>
2024-04-03 18:14:21 +0200

    jks files deleted and import script expanded

--- back-end/pki-system/src/main/resources/import.sql
+++ back-end/pki-system/src/main/resources/import.sql
@@ -4,6 +4,10 @@ INSERT INTO public.users (id, common_name, country, given_name, mail, organizati
 INSERT INTO public.users (id, common_name, country, given_name, mail, organization, organization_unit, password, surname) VALUES (4,'Tasa Tasic', 'Serbia', 'Tasa', 'tasa@gmail.com', 'PKI4', 'PKI-Sabac', '$2a$10$lhYgvOwC1Q.fxzQBkwVNI.xqwKaoQiY6Gum5fzeN9jsuYStzORNGi', 'Tasa');
 INSERT INTO public.users (id, common_name, country, given_name, mail, organization, organization_unit, password, surname) VALUES (5,'Andja Andjic', 'Serbia', 'Andja', 'andja@gmail.com', 'PKI5', 'PKI-Lazarevac', '$2a$10$lhYgvOwC1Q.fxzQBkwVNI.xqwKaoQiY6Gum5fzeN9jsuYStzORNGi', 'Andja');
 
+INSERT INTO public.users (id, common_name, country, given_name, mail, organization, organization_unit, password, surname) VALUES (6,'Jovan Milnekovic', 'Serbia', 'Jovan', 'jovan@gmail.com', 'PKI6', 'PKI-Mokra-Gora', '$2a$10$lhYgvOwC1Q.fxzQBkwVNI.xqwKaoQiY6Gum5fzeN9jsuYStzORNGi', 'Jovan');
+INSERT INTO public.users (id, common_name, country, given_name, mail, organization, organization_unit, password, surname) VALUES (7,'Danilo Radivojevic', 'Serbia', 'Danilo', 'danilo@gmail.com', 'PKI7', 'PKI-Mokra-Gora', '$2a$10$lhYgvOwC1Q.fxzQBkwVNI.xqwKaoQiY6Gum5fzeN9jsuYStzORNGi', 'Danilo');
+INSERT INTO public.users (id, common_name, country, given_name, mail, organization, organization_unit, password, surname) VALUES (8,'Veljko Vukotic', 'Serbia', 'Veljko', 'veljko@gmail.com', 'PKI8', 'PKI-Dreznik', '$2a$10$lhYgvOwC1Q.fxzQBkwVNI.xqwKaoQiY6Gum5fzeN9jsuYStzORNGi', 'Veljko');
+
 
 INSERT INTO public.role (id, name) VALUES (1,'ADMIN');
 INSERT INTO public.role (id, name) VALUES (2,'USER');
@@ -14,3 +18,6 @@ INSERT INTO user_role (user_id, role_id) VALUES (2,2);
 INSERT INTO user_role (user_id, role_id) VALUES (3,2);
 INSERT INTO user_role (user_id, role_id) VALUES (4,2);
 INSERT INTO user_role (user_id, role_id) VALUES (5,2);
+INSERT INTO user_role (user_id, role_id) VALUES (6,2);
+INSERT INTO user_role (user_id, role_id) VALUES (7,2);
+INSERT INTO user_role (user_id, role_id) VALUES (8,2);
